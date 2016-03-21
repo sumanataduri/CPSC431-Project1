44c3de9c2e50e6655b693c9e2e29e9ac9a42adfd
Sumanataduri <sumanataduri@gmail.com>
2016-03-20 23:14:23 -0700

    commented my post code

--- reddit.js
+++ reddit.js
@@ -8,39 +8,76 @@ var main = function() {
                 var postsList = "<div class ='postContent'>" + "<div class = 'votes'>" + "<img class='voteUpButton' src='image/up.png'>" + "<br>" + "<strong id =" + reddit.id + " class='votesNum'>" + "</strong>" + "<br>" + "<img class='voteDownButton' src='image/down.png'>" + "</div>" + "<div class ='Content-List'>" + "<p>" + "<a href=" + JSON.stringify(reddit.main_link) + ">" + reddit.link_title + "</a>" + "</p>" + "</div>";
 
                 $(postsList).appendTo('div.postsContainer');
-                $("#" + reddit.id + "").text(reddit.likes);
+                $("#" + reddit.id + ".votesNum").text(reddit.likes);
+                $("#postform")[0].reset(); //For posting area
             });
         });
     }
+    //Start- for postform validation
     myfunction();
+    jQuery.validator.setDefaults({
+        debug: true,
+        success: "valid"
+    });
+    $("#postform").validate({
+        rules: {
+            field: {
+                required: true,
+                url: true
+            }
+        }
+    });
+    var form = $("#postform");
+    form.validate();//End- post form validation
+    //Start- JQuery code for post fields
     $("#postbutton").click(function(element) {
         var check = $("#checkbox").is(':checked');
         if (check === true) {
             if ($("#input1").val() === "" || $("#input2").val() == "" || $("#input3").val() == "") {
                 element.preventDefault();
+                setTimeout(fade_out, 5000);
                 $("#spanbutton").css({
-                    "visibility": "visible"
+                    "visibility": "visible",
+                    "display": "inline"
                 }).text("Enter input");
+
+                function fade_out() {
+                    $("#spanbutton").fadeOut().empty();
+                }
             } else {
-                alert("going to post")
-                //$("#spanbutton").css({"visibility": "visible"}).text("");
-                $.post("http://localhost:3000/reddit", {
-                    "link_title": $("#input1").val(),
-                    "main_link": $("#input2").val(),
-                    "image_link": $("#input3").val(),
-                    "likes": 0,
-                    "post": "submitted"
-                },function(){
-                    alert("success");
-                });
-           }
+                if (form.valid() === true) {
+
+                    $("#spanbutton").css({
+                        "visibility": "visible"
+                    }).text("");
+                    $.post("http://localhost:3000/reddit", {
+                        "link_title": $("#input1").val(),
+                        "main_link": $("#input2").val(),
+                        "image_link": $("#input3").val(),
+                        "likes": 0,
+                        "post": "submitted"
+                    }, function() {
+                        myfunction();
+
+                    });
+
+                }
+
+            }
         } else {
             element.preventDefault();
+            setTimeout(fade_out, 5000);
+
+            function fade_out() {
+                $("#spanbutton").fadeOut().empty();
+            }
             $("#spanbutton").css({
-                "visibility": "visible"
+                "visibility": "visible",
+                "display": "inline"
             }).text("Log in to post");
         }
     });
-}
+    //End-code for post fields
+};
 
 $(document).ready(main);
